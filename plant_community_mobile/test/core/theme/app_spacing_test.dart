e8068b36e870680947864cd2c969087549056eac
xertox1234 <william.tower@gmail.com>
2026-05-31 15:25:55 -0600

    feat: add Green Thumb radius tokens to AppSpacing

---
+++ plant_community_mobile/test/core/theme/app_spacing_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:plant_community_mobile/core/constants/app_spacing.dart';
+
+void main() {
+  group('AppSpacing radius tokens', () {
+    test('rXs is 6', () => expect(AppSpacing.rXs, 6.0));
+    test('rSm is 10', () => expect(AppSpacing.rSm, 10.0));
+    test('rMd is 16', () => expect(AppSpacing.rMd, 16.0));
+    test('rLg is 22', () => expect(AppSpacing.rLg, 22.0));
+    test('rXl is 28', () => expect(AppSpacing.rXl, 28.0));
+    test('rPill is 999', () => expect(AppSpacing.rPill, 999.0));
+  });
+}
